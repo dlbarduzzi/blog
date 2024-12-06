13b1b6af3c01335e22bc191e8fafa64c79ce47c8
Dylan Barduzzi <dlbarduzzi@gmail.com>
2024-12-06 10:06:09 -0600

    Add initial content-collections setup

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,23 @@
+import { allPosts } from "content-collections"
+
 export default function Page() {
+  console.log(allPosts)
   return (
-    <div className="text-red bg-white">
+    <div className="bg-white">
       <div className="py-8">
         <div className="text-center">Welcome to my blog!</div>
+      </div>
+      <div className="mx-auto max-w-7xl px-4">
+        <div className="space-y-6">
+          {allPosts.map(post => (
+            <div
+              key={post.title}
+              className="rounded-md border border-gray-300 px-4 py-5"
+            >
+              <h3>{post.title}</h3>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
