322dacb88f21b3c13647259111f69cc3457ce09a
Dylan Barduzzi <dlbarduzzi@gmail.com>
2024-12-10 13:30:06 -0600

    Add logo component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,31 +1,14 @@
 import Link from "next/link"
 
 import { allPosts } from "content-collections"
+import { Container } from "@/components/container"
 import { ToggleTheme } from "@/components/toggle-theme"
 
 export default function Page() {
   return (
-    <div>
-      <div className="py-8">
-        <div className="text-center">Welcome to my blog!</div>
-      </div>
-      <div className="mx-auto max-w-7xl px-4">
+    <div className="py-8">
+      <Container>
         <ToggleTheme />
-        <div className="mt-8 flex flex-wrap items-center gap-2">
-          <div className="bg-red px-3 py-2">Color</div>
-          <div className="bg-yellow px-3 py-2">Color</div>
-          <div className="bg-blue px-3 py-2">Color</div>
-          <div className="bg-orange px-3 py-2">Color</div>
-          <div className="bg-green px-3 py-2">Color</div>
-          <div className="bg-magenta px-3 py-2">Color</div>
-          <div className="bg-gray1 px-3 py-2">Color</div>
-          <div className="bg-gray2 px-3 py-2">Color</div>
-          <div className="bg-gray3 px-3 py-2">Color</div>
-          <div className="bg-gray4 px-3 py-2">Color</div>
-          <div className="bg-gray5 px-3 py-2">Color</div>
-          <div className="bg-gray6 px-3 py-2">Color</div>
-          <div className="bg-gray7 px-3 py-2">Color</div>
-        </div>
         <div className="mt-8 space-y-6">
           {allPosts.map(post => (
             <Link
@@ -46,7 +29,7 @@ export default function Page() {
             </Link>
           ))}
         </div>
-      </div>
+      </Container>
     </div>
   )
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,19 @@
+import Link from "next/link"
+
+import { Logo } from "./logo"
 import { Container } from "@/components/container"
 
 export function Header() {
   return (
     <header className="border-b border-b-gray1 bg-background">
       <Container>
-        <nav className="flex h-14 items-center">Navbar</nav>
+        <nav className="flex h-14 items-center">
+          <div>
+            <Link href="/">
+              <Logo />
+            </Link>
+          </div>
+        </nav>
       </Container>
     </header>
   )
